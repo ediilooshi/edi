e176f6b5b66e7207d26fa4f6da6c3df08f2d9611
CodeJourney <140146215+ediilooshi@users.noreply.github.com>
2024-04-28 23:37:52 +0800

    Update Deployment Exercise.sol

--- Deployment Exercise.sol
+++ Deployment Exercise.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract BasicMath {
-		string private salt = "28"; 
+    string public salt = "28"; 
+
     function adder(uint _a, uint _b) external pure returns (uint sum, bool error) {
         unchecked {
             uint result = _a + _b;
@@ -18,6 +19,27 @@ contract BasicMath {
         unchecked {
             if (_b <= _a) {
                 return (_a - _b, false);
+            } else {
+                return (0, true);
+            }
+        }
+    }
+
+    function multiplier(uint _a, uint _b) external pure returns (uint product, bool error) {
+        unchecked {
+            uint result = _a * _b;
+            if (_a == 0 || _b == 0 || result / _a == _b) {
+                return (result, false);
+            } else {
+                return (0, true);
+            }
+        }
+    }
+
+    function divider(uint _a, uint _b) external pure returns (uint quotient, bool error) {
+        unchecked {
+            if (_b != 0 && _a >= _b) {
+                return (_a / _b, false);
             } else {
                 return (0, true);
             }
